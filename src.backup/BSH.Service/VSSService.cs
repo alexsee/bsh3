cb2f91f3d3a284b78260765ec5af85204f0bad05
Alexander Seeliger <alexander@brightbits.de>
2023-01-11 19:42:06 +0100

    chore: upgrade service project

---
+++ src.backup/BSH.Service/VSSService.cs
@@ -0,0 +1,108 @@
+﻿// Copyright 2022 Alexander Seeliger
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using BSH.Service.Shared;
+using BSH.Service.VSS;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace BSH.Service
+{
+    public class VSSService : IVSSRemoteObject
+    {
+        private Exception exception;
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CopyFileEx(string lpExistingFileName, string lpNewFileName,
+           CopyProgressRoutine lpProgressRoutine, IntPtr lpData, ref Int32 pbCancel,
+           CopyFileFlags dwCopyFlags);
+
+        delegate CopyProgressResult CopyProgressRoutine(
+            long TotalFileSize,
+            long TotalBytesTransferred,
+            long StreamSize,
+            long StreamBytesTransferred,
+            uint dwStreamNumber,
+            CopyProgressCallbackReason dwCallbackReason,
+            IntPtr hSourceFile,
+            IntPtr hDestinationFile,
+            IntPtr lpData);
+
+        int pbCancel;
+
+        enum CopyProgressResult : uint
+        {
+            PROGRESS_CONTINUE = 0,
+            PROGRESS_CANCEL = 1,
+            PROGRESS_STOP = 2,
+            PROGRESS_QUIET = 3
+        }
+
+        enum CopyProgressCallbackReason : uint
+        {
+            CALLBACK_CHUNK_FINISHED = 0x00000000,
+            CALLBACK_STREAM_SWITCH = 0x00000001
+        }
+
+        [Flags]
+        enum CopyFileFlags : uint
+        {
+            COPY_FILE_FAIL_IF_EXISTS = 0x00000001,
+            COPY_FILE_RESTARTABLE = 0x00000002,
+            COPY_FILE_OPEN_SOURCE_FOR_WRITE = 0x00000004,
+            COPY_FILE_ALLOW_DECRYPTED_DESTINATION = 0x00000008
+        }
+
+        private void XCopy(string oldFile, string newFile)
+        {
+            CopyFileEx(oldFile, newFile, new CopyProgressRoutine(this.CopyProgressHandler), IntPtr.Zero, ref pbCancel, CopyFileFlags.COPY_FILE_RESTARTABLE);
+        }
+
+        private CopyProgressResult CopyProgressHandler(long total, long transferred, long streamSize, long StreamByteTrans, uint dwStreamNumber, CopyProgressCallbackReason reason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData)
+        {
+            return CopyProgressResult.PROGRESS_CONTINUE;
+        }
+
+        public bool CopyFileWithVSS(string vssServiceFolder, string source, string destination)
+        {
+            var fileInfo = new FileInfo(vssServiceFolder);
+
+            try
+            {
+                using (var vss = new VssBackup())
+                {
+                    vss.Setup(fileInfo.Directory.Root.Name);
+
+                    var snapshotPath = vss.GetSnapshotPath(source);
+                    XCopy(snapshotPath, destination);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.exception = ex;
+            }
+
+            return false;
+        }
+
+        public Exception GetException()
+        {
+            return this.exception;
+        }
+    }
+}
