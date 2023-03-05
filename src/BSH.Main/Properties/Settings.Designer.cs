b8ebd7f3c19bb50c67712fa8c152a4ca2167a17e
Alexander Seeliger <alexander@brightbits.de>
2023-03-05 11:49:51 +0100

    feat(browser): add support for remembering column size

--- src/BSH.Main/Properties/Settings.Designer.cs
+++ src/BSH.Main/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace BSH.Main.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -44,30 +44,6 @@ namespace BSH.Main.Properties {
             }
             set {
                 this["SystemConfigured"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool BrowserVersionsCollapse {
-            get {
-                return ((bool)(this["BrowserVersionsCollapse"]));
-            }
-            set {
-                this["BrowserVersionsCollapse"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool BrowserFavoritsCollapse {
-            get {
-                return ((bool)(this["BrowserFavoritsCollapse"]));
-            }
-            set {
-                this["BrowserFavoritsCollapse"] = value;
             }
         }
         
@@ -169,18 +145,6 @@ namespace BSH.Main.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool FirstStartWelcomeCenter {
-            get {
-                return ((bool)(this["FirstStartWelcomeCenter"]));
-            }
-            set {
-                this["FirstStartWelcomeCenter"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string StartParameters {
             get {
@@ -200,6 +164,18 @@ namespace BSH.Main.Properties {
             }
             set {
                 this["UniqueUserId"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string BrowserColumnsSize {
+            get {
+                return ((string)(this["BrowserColumnsSize"]));
+            }
+            set {
+                this["BrowserColumnsSize"] = value;
             }
         }
     }
