795466a752f65103938423d4071800cf204741ea
Alexander Seeliger <alexander@brightbits.de>
2023-08-27 14:19:36 +0200

    fix(core): correct path to source file

--- src/BSH.Service/VSSService.cs
+++ src/BSH.Service/VSSService.cs
@@ -22,63 +22,11 @@ namespace BSH.Service
 {
     public class VSSService : IVSSRemoteObject
     {
-        private Exception exception;
-
-        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool CopyFileEx(string lpExistingFileName, string lpNewFileName,
-           CopyProgressRoutine lpProgressRoutine, IntPtr lpData, ref Int32 pbCancel,
-           CopyFileFlags dwCopyFlags);
-
-        delegate CopyProgressResult CopyProgressRoutine(
-            long TotalFileSize,
-            long TotalBytesTransferred,
-            long StreamSize,
-            long StreamBytesTransferred,
-            uint dwStreamNumber,
-            CopyProgressCallbackReason dwCallbackReason,
-            IntPtr hSourceFile,
-            IntPtr hDestinationFile,
-            IntPtr lpData);
-
-        int pbCancel;
-
-        enum CopyProgressResult : uint
-        {
-            PROGRESS_CONTINUE = 0,
-            PROGRESS_CANCEL = 1,
-            PROGRESS_STOP = 2,
-            PROGRESS_QUIET = 3
-        }
-
-        enum CopyProgressCallbackReason : uint
-        {
-            CALLBACK_CHUNK_FINISHED = 0x00000000,
-            CALLBACK_STREAM_SWITCH = 0x00000001
-        }
-
-        [Flags]
-        enum CopyFileFlags : uint
-        {
-            COPY_FILE_FAIL_IF_EXISTS = 0x00000001,
-            COPY_FILE_RESTARTABLE = 0x00000002,
-            COPY_FILE_OPEN_SOURCE_FOR_WRITE = 0x00000004,
-            COPY_FILE_ALLOW_DECRYPTED_DESTINATION = 0x00000008
-        }
-
-        private void XCopy(string oldFile, string newFile)
-        {
-            CopyFileEx(oldFile, newFile, new CopyProgressRoutine(this.CopyProgressHandler), IntPtr.Zero, ref pbCancel, CopyFileFlags.COPY_FILE_RESTARTABLE);
-        }
-
-        private CopyProgressResult CopyProgressHandler(long total, long transferred, long streamSize, long StreamByteTrans, uint dwStreamNumber, CopyProgressCallbackReason reason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData)
-        {
-            return CopyProgressResult.PROGRESS_CONTINUE;
-        }
+        private Exception exception = null;
 
         public bool CopyFileWithVSS(string vssServiceFolder, string source, string destination)
         {
-            var fileInfo = new FileInfo(vssServiceFolder);
+            var fileInfo = new FileInfo(source);
 
             try
             {
@@ -87,7 +35,7 @@ namespace BSH.Service
                     vss.Setup(fileInfo.Directory.Root.Name);
 
                     var snapshotPath = vss.GetSnapshotPath(source);
-                    XCopy(snapshotPath, destination);
+                    XCopy.Copy(snapshotPath, destination, true, false);
                 }
 
                 return true;
