8cd785ad68552cfc096c238245f0038401175947
Alexander Seeliger <alexander@brightbits.de>
2023-08-27 14:21:00 +0200

    chore(build): update versions

--- src/BSH.Engine/Properties/AssemblyInfo.cs
+++ src/BSH.Engine/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.8.5.2")]
-[assembly: AssemblyFileVersion("3.8.5.2")]
+[assembly: AssemblyVersion("3.8.5.3")]
+[assembly: AssemblyFileVersion("3.8.5.3")]
 
