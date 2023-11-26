3c7b16bbd89a46cdec9cbeef2056c13319d0441f
Alexander Seeliger <alexsee@users.noreply.github.com>
2023-11-26 16:58:04 +0100

    feat(core): add new software updater (#255)

--- src/BSH.Main/Program.cs
+++ src/BSH.Main/Program.cs
@@ -12,14 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using AutoUpdaterDotNET;
 using BSH.Main.Model.CommandLine;
 using BSH.Main.Properties;
 using CommandLine;
 using Serilog;
 using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Brightbits.BSH.Main;
@@ -29,36 +28,6 @@ static class Program
     public const string APP_TITLE = "Backup Service Home";
 
     private static System.Threading.Mutex mutex;
-
-#if !WIN_UWP
-    private static updateSystemDotNet.updateController _mainUpdateController;
-
-    public static updateSystemDotNet.updateController mainUpdateController
-    {
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        get
-        {
-            return _mainUpdateController;
-        }
-
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        set
-        {
-            if (_mainUpdateController != null)
-            {
-                _mainUpdateController.updateFound -= mainUpdateController_updateFound;
-                _mainUpdateController.updateInstallerStarted -= mainUpdateController_updateInstallerStarted;
-            }
-
-            _mainUpdateController = value;
-            if (_mainUpdateController != null)
-            {
-                _mainUpdateController.updateFound += mainUpdateController_updateFound;
-                _mainUpdateController.updateInstallerStarted += mainUpdateController_updateInstallerStarted;
-            }
-        }
-    }
-#endif
 
     public static string CurrentVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
@@ -93,32 +62,10 @@ static class Program
             NotificationController.Current.InitializeSystemTrayIcon();
             Microsoft.Win32.SystemEvents.SessionEnding += Shutdown;
 
-#if !WIN_UWP
-            // init UpdateSystem
-            mainUpdateController = new updateSystemDotNet.updateController
-            {
-                applicationLocation = "",
-                projectId = "0d342c3a-392f-417e-974f-ca0715d39283",
-                proxyPassword = null,
-                proxyUrl = null,
-                proxyUsername = null,
-                publicKey = "<RSAKeyValue><Modulus>uX5wAIeaOlLNQZ/X3+0vtwf4OZJwjm9odouMWhyQSatjL8marLBGlaSfRYMjP1MxYOdUtJl6z4MkfNQ6K18qoKj0NPxgZAfpofUou7zZRVoUEFV8exLTi5WYacYPP7N3ujC4DkNPbd9335CQSa9RAfzDflVS1YKjH7SV0vN86E0OIHvokfBLOEf5KwLC/mlDpMh7XDAGZXcDwP8Hu29ZBf8BcJfyjlYFgK+EVzPdm8KWdM1zwIkBeJkZ/87NNTEsQiFz5lHAWI1MmfGR3aGgTWyqlPoWLKaX+nFLMmQqFwjTx7r2KIoTdka5Nv/GMEoHqAntPU5F39D3IppnthHJODJLhncoF7Dx5rQgrCbMRKfvkjOZ/VwxwFA22k5kIcnxWIIpmDb51rlcVF2nYjR3e5Z4wqRMKNeMUenCuyD7Yi7TxQGAfLy6CS6p138SdTJ8dZ07HtPt55V6cQmAzYkJL4Pw7pL/4d+Lqs3Mdp6qAjF1VO8JIeIT9/XO9I59H3aL8qFKLuKLHqF7x2Lrjitsk1PJh/cjHWv0DVWth96SzG5rV9DHwhzZf8hCwWmn4Nw1mF2d2pCxAB46Y/ee0Pzu5QWxN3qbomhllroZrGz7uxvqrOwsCCf4duE5Vhv7JbFVO6288izWvFaIjysf1SskJEbxseG1DFiD/yZYitSPIlE=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>",
-                requestElevation = true,
-                updateUrl = "https://updates.brightbits.de/_bsh_3",
-                restartApplication = true,
-            };
-            mainUpdateController.releaseInfo.Version = Application.ProductVersion.Replace("-beta", ".");
-
-            if (Settings.Default.AutoSearchUpdates)
-            {
-                mainUpdateController.updateCheckInterval = updateSystemDotNet.Interval.Weekly;
-                mainUpdateController.checkForUpdatesAsync();
-            }
-            else
-            {
-                mainUpdateController.updateCheckInterval = updateSystemDotNet.Interval.Custom;
-            }
-#endif
+            // setup software updater
+            AutoUpdater.ApplicationExitEvent += AutoUpdater_ApplicationExitEvent;
+            AutoUpdater.HttpUserAgent = $"{APP_TITLE}/{CurrentVersion}";
+            AutoUpdater.TopMost = true;
 
             // start backup engine
             BackupLogic.StartupAsync().Wait();
@@ -137,6 +84,27 @@ static class Program
         catch (Exception ex)
         {
             ExceptionController.HandleGlobalException(null, new System.Threading.ThreadExceptionEventArgs(ex));
+        }
+    }
+
+    private static void AutoUpdater_ApplicationExitEvent()
+    {
+        // close application
+        NotificationController.Current.Shutdown();
+        BackupLogic.StopSystem();
+        Settings.Default.StartParameters = Environment.CommandLine;
+        Settings.Default.Save();
+
+        PresentationController.Current.CloseMainWindow();
+        PresentationController.Current.CloseBackupBrowserWindow();
+
+        try
+        {
+            Application.Exit();
+        }
+        catch
+        {
+            Environment.Exit(0);
         }
     }
 
@@ -247,32 +215,4 @@ static class Program
             ShowBackupbrowserWindowExitCode();
         }
     }
-
-#if !WIN_UWP
-    private static void mainUpdateController_updateFound(object sender, updateSystemDotNet.appEventArgs.updateFoundEventArgs e)
-    {
-        NotificationController.Current.ShowIconBalloon(5000, Resources.INFO_BSH_UPDATE_AVAILABLE_TITLE, Resources.INFO_BSH_UPDATE_AVAILABLE_TEXT, ToolTipIcon.Info);
-    }
-
-    private static void mainUpdateController_updateInstallerStarted(object sender, updateSystemDotNet.appEventArgs.updateInstallerStartedEventArgs e)
-    {
-        // close application
-        NotificationController.Current.Shutdown();
-        BackupLogic.StopSystem();
-        Settings.Default.StartParameters = Environment.CommandLine;
-        Settings.Default.Save();
-
-        PresentationController.Current.CloseMainWindow();
-        PresentationController.Current.CloseBackupBrowserWindow();
-
-        try
-        {
-            Application.Exit();
-        }
-        catch
-        {
-            Environment.Exit(0);
-        }
-    }
-#endif
 }
