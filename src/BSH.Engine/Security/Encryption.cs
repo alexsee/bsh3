7af6a8c7506962802ddf74b4f5438142b9ac6744
Alexander Seeliger <alexander@brightbits.de>
2024-02-11 19:02:27 +0100

    feat(encryption): update to latest encryption classes

--- src/BSH.Engine/Security/Encryption.cs
+++ src/BSH.Engine/Security/Encryption.cs
@@ -19,25 +19,12 @@ using System.Security.Cryptography;
 
 namespace Brightbits.BSH.Engine.Security;
 
-/// <summary>
-/// ''' Das ist eine Kryptographieklasse die die Verschlüsselungsalgorithmen aus den .NET Framework
-/// ''' zusammenfasst und in einfach anzuwendenen Funktionen kapselt.
-/// ''' </summary>
-/// ''' <remarks>Tim Hartwig</remarks>
 public class Encryption
 {
-    private readonly byte[] mKeySalt = new byte[] { 0xA1, 0x41, 0xC4, 0xF5, 0x23, 0x70, 0xBF, 0x52 };
-    private readonly byte[] mIVSalt = new byte[] { 0x47, 0x80, 0x22, 0xFF, 0x12, 0xE7, 0xF1, 0x39 };
+    private readonly byte[] mKeySalt = [0xA1, 0x41, 0xC4, 0xF5, 0x23, 0x70, 0xBF, 0x52];
+    private readonly byte[] mIVSalt = [0x47, 0x80, 0x22, 0xFF, 0x12, 0xE7, 0xF1, 0x39];
     private int mBitLen;
 
-    public event ProgressErrorEventHandler ProgressError;
-
-    public delegate void ProgressErrorEventHandler(object sender, Exception ex);
-
-    /// <summary>
-    ///     ''' Gibt die Verschlüsselungsstärke in Bits an:
-    ///     ''' DES = 64;  TDES = 128,192;  RIJNDAEL = 128,192,256;  RC2 = 40 bis 128 (8er Schritte)
-    ///     ''' </summary>
     public int BitLen
     {
         get
@@ -55,14 +42,6 @@ public class Encryption
         }
     }
 
-    public enum ALGO
-    {
-        /// <summary>
-        ///         ''' 128 Bit, 192 Bit, 256 Bit
-        ///         ''' </summary>
-        RIJNDAEL = 2
-    }
-
     private void CheckBitLen()
     {
         if (mBitLen == 0 || mBitLen != 16 || mBitLen != 24 || mBitLen != 32)
@@ -71,110 +50,64 @@ public class Encryption
         }
     }
 
-    /// <summary>
-    ///      Diese Funktion verschlüsselt eine Datei und speichert es in eine andere Datei (HDD -> HDD)
-    ///     </summary>
     public bool Encode(string SourceFile, string TargetFile, SecureString Password, int BufferSize = 4096)
     {
         try
         {
-            using (var InFileStream = new FileStream(SourceFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
-                using (FileStream OutFileStream = new(TargetFile, FileMode.Create))
-                {
-                    CryptoStream CryptStream = null;
-
-                    var Data = new byte[BufferSize - 1 + 1];
-                    long Buffer;
-                    long BufferCount = 0;
-                    var FileLen = InFileStream.Length;
-
-                    Rfc2898DeriveBytes MakeKey = new(Crypto.ToInsecureString(Password), mKeySalt);
-                    Rfc2898DeriveBytes MakeIV = new(Crypto.ToInsecureString(Password), mIVSalt);
-
-                    // Prüfen ob die Bitstärke mit dem gewählten Algorithmus übereinstimmt und evtl. anpassen
-                    CheckBitLen();
-
-                    RijndaelManaged RIJNDAEL = new();
-                    CryptStream = new CryptoStream(OutFileStream, RIJNDAEL.CreateEncryptor(MakeKey.GetBytes(mBitLen), MakeIV.GetBytes(16)), CryptoStreamMode.Write); // 16,24,32
-                    do
-                    {
-                        if (BufferCount >= FileLen - BufferSize)
-                        {
-                            Data = new byte[Convert.ToInt32(FileLen - BufferCount) + 1];
-                            Buffer = InFileStream.Read(Data, 0, Convert.ToInt32(FileLen - BufferCount));
-                            CryptStream.Write(Data, 0, Convert.ToInt32(Buffer));
-                            break;
-                        }
-
-                        Buffer = InFileStream.Read(Data, 0, BufferSize);
-                        CryptStream.Write(Data, 0, BufferSize);
-                        BufferCount += Buffer;
-                    }
-                    while (true);
-
-                    CryptStream.Close();
-                }
-            }
-
-            return true;
-        }
-        catch (Exception ex)
-        {
-            ProgressError?.Invoke(this, ex);
-            return false;
-        }
-    }
-
-    /// <summary>
-    ///     ''' Diese Funktion entschlüsselt eine Datei und speichert es in eine andere Datei (HDD -> HDD)
-    ///     ''' </summary>
-    public bool Decode(string SourceFile, string TargetFile, SecureString Password, int BufferSize = 4096)
-    {
-        try
-        {
-            FileStream InFileStream = new(SourceFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            FileStream OutFileStream = new(TargetFile, FileMode.Create);
-            CryptoStream CryptStream = null;
-
-            var Data = new byte[BufferSize - 1 + 1];
-            long Buffer;
-            long BufferCount = 0;
-            var FileLen = InFileStream.Length;
+            using var InFileStream = new FileStream(SourceFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using FileStream OutFileStream = new(TargetFile, FileMode.Create);
 
             Rfc2898DeriveBytes MakeKey = new(Crypto.ToInsecureString(Password), mKeySalt);
             Rfc2898DeriveBytes MakeIV = new(Crypto.ToInsecureString(Password), mIVSalt);
 
-            // Prüfen ob die Bitstärke mit dem gewählten Algorithmus übereinstimmt und evtl. anpassen
             CheckBitLen();
 
-            RijndaelManaged RIJNDAEL = new();
-            CryptStream = new CryptoStream(InFileStream, RIJNDAEL.CreateDecryptor(MakeKey.GetBytes(mBitLen), MakeIV.GetBytes(16)), CryptoStreamMode.Read);
+            var aes = Aes.Create();
+            using var CryptStream = new CryptoStream(OutFileStream, aes.CreateEncryptor(MakeKey.GetBytes(mBitLen), MakeIV.GetBytes(16)), CryptoStreamMode.Write); // 16,24,32
 
-            do
+            var buffer = new byte[BufferSize];
+            int read;
+
+            while ((read = InFileStream.Read(buffer, 0, buffer.Length)) > 0)
             {
-                if (BufferCount >= FileLen - BufferSize)
-                {
-                    Data = new byte[Convert.ToInt32(FileLen - BufferCount) + 1];
-                    Buffer = CryptStream.Read(Data, 0, Convert.ToInt32(FileLen - BufferCount));
-                    OutFileStream.Write(Data, 0, Convert.ToInt32(Buffer));
-                    break;
-                }
-
-                Buffer = CryptStream.Read(Data, 0, BufferSize);
-                OutFileStream.Write(Data, 0, BufferSize);
-                BufferCount += Buffer;
+                CryptStream.Write(buffer, 0, read);
             }
-            while (true);
 
-            OutFileStream.Close();
-            CryptStream.Close();
-            InFileStream.Close();
             return true;
         }
-        catch (Exception ex)
+        catch
         {
-            ProgressError?.Invoke(this, ex);
+            return false;
+        }
+    }
+
+    public bool Decode(string SourceFile, string TargetFile, SecureString Password, int BufferSize = 4096)
+    {
+        try
+        {
+            using var InFileStream = new FileStream(SourceFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using var OutFileStream = new FileStream(TargetFile, FileMode.Create);
+
+            Rfc2898DeriveBytes MakeKey = new(Crypto.ToInsecureString(Password), mKeySalt);
+            Rfc2898DeriveBytes MakeIV = new(Crypto.ToInsecureString(Password), mIVSalt);
+
+            CheckBitLen();
+
+            var aes = Aes.Create();
+            using var CryptStream = new CryptoStream(InFileStream, aes.CreateDecryptor(MakeKey.GetBytes(mBitLen), MakeIV.GetBytes(16)), CryptoStreamMode.Read);
+
+            var buffer = new byte[BufferSize];
+            int read;
+
+            while ((read = CryptStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                OutFileStream.Write(buffer, 0, read);
+            }
+
+            return true;
+        }
+        catch
+        {
             return false;
         }
     }
