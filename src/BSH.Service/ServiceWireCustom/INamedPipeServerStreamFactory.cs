7adee9778aef907e184ab35313f76dcdee137a19
Alexander Seeliger <alexsee@users.noreply.github.com>
2024-12-30 16:21:50 +0100

    chore: code cleanup and package updates. (#351)

--- src/BSH.Service/ServiceWireCustom/INamedPipeServerStreamFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO.Pipes;
-
-namespace ServiceWire.NamedPipes
-{
-    public interface INamedPipeServerStreamFactory
-    {
-        NamedPipeServerStream Create(string pipeName, PipeDirection direction, int maxNumberOfServerInstances, PipeTransmissionMode transmissionMode, PipeOptions options, int inBufferSize, int outBufferSize);
-    }
-}
