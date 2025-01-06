c463fb539b9d85a2c79f481bff5805aee53d9569
Alexander Seeliger <alexander@brightbits.de>
2025-01-06 17:03:21 +0100

    feat(dlg): add new backup window

---
+++ src/BSH.MainApp/Windows/NewBackupWindow.xaml.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Alexander Seeliger. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+
+namespace BSH.MainApp.Windows;
+
+public sealed partial class NewBackupWindow : WinUIEx.WindowEx
+{
+    public NewBackupWindow()
+    {
+        this.InitializeComponent();
+    }
+}
