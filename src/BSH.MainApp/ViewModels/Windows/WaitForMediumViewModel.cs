7dc3fb922720bdac1d5313f8e8ed437057460873
Alexander Seeliger <alexsee@users.noreply.github.com>
2025-01-11 17:43:39 +0100

    feat(dlg): add wait for media window (#355)

---
+++ src/BSH.MainApp/ViewModels/Windows/WaitForMediumViewModel.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Alexander Seeliger. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+
+using CommunityToolkit.Mvvm.Input;
+
+namespace BSH.MainApp.ViewModels.Windows;
+
+public partial class WaitForMediumViewModel
+{
+    public event OnCancelRequestedEventHandler OnCancelRequested;
+
+    public delegate void OnCancelRequestedEventHandler();
+
+    [RelayCommand]
+    private void Cancel()
+    {
+        OnCancelRequested?.Invoke();
+    }
+}
---
+++ src/BSH.MainApp/Windows/WaitForMediumWindow.xaml.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Alexander Seeliger. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+
+using BSH.MainApp.ViewModels.Windows;
+
+namespace BSH.MainApp.Windows;
+
+public sealed partial class WaitForMediumWindow : WinUIEx.WindowEx
+{
+    public WaitForMediumViewModel ViewModel { get; } = new WaitForMediumViewModel();
+
+    public WaitForMediumWindow()
+    {
+        this.InitializeComponent();
+    }
+
+    private void WindowEx_Closed(object sender, Microsoft.UI.Xaml.WindowEventArgs args)
+    {
+        ViewModel.CancelCommand.Execute(null);
+    }
+}
