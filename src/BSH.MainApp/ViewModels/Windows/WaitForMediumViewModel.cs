401c188d9c7c39327732a5ed96ebdebefa71e27c
Alexander Seeliger <alexander@brightbits.de>
2025-01-11 17:24:02 +0100

    feat(dlg): add wait for media window

---
+++ src/BSH.MainApp/ViewModels/Windows/WaitForMediumViewModel.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Alexander Seeliger. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+
+using CommunityToolkit.Mvvm.Input;
+
+namespace BSH.MainApp.ViewModels.Windows;
+
+public partial class WaitForMediumViewModel
+{
+    public event EventHandler? OnCancelRequested;
+
+    [RelayCommand]
+    private void Cancel()
+    {
+        OnCancelRequested?.Invoke(this, EventArgs.Empty);
+    }
+}
