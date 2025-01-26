505b4ef07139aeda9ada34c297479531e1adc6b5
Alexander Seeliger <alexander@brightbits.de>
2025-01-26 18:17:21 +0100

    feat(StatusWindow): wire up status window

--- src/BSH.MainApp/ViewModels/Windows/StatusViewModel.cs
+++ src/BSH.MainApp/ViewModels/Windows/StatusViewModel.cs
@@ -1,10 +1,84 @@
 ﻿// Copyright (c) Alexander Seeliger. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0.
 
+using Brightbits.BSH.Engine;
+using Brightbits.BSH.Engine.Contracts.Services;
+using Brightbits.BSH.Engine.Jobs;
+using BSH.MainApp.Contracts;
+using BSH.MainApp.Contracts.Services;
+using BSH.MainApp.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Dispatching;
 
 namespace BSH.MainApp.ViewModels.Windows;
-public class StatusViewModel : ObservableObject
+public partial class StatusViewModel : ObservableObject, IStatusReport
 {
+    private readonly DispatcherQueue dispatcherQueue;
 
+    [ObservableProperty]
+    private string statusTitle = "";
+
+    [ObservableProperty]
+    private string statusText = "";
+
+    [ObservableProperty]
+    private string currentFileText = "";
+
+    [ObservableProperty]
+    private int totalProgress = 100;
+
+    [ObservableProperty]
+    private int currentProgress = 0;
+
+    public StatusViewModel()
+    {
+        this.dispatcherQueue = App.GetService<DispatcherQueue>();
+    }
+
+    public void ReportAction(ActionType action, bool silent)
+    {
+        // not used
+    }
+
+    public void ReportState(JobState jobState)
+    {
+        // not used
+    }
+    public void ReportSystemStatus(SystemStatus systemStatus)
+    {
+        // not used
+    }
+
+    public void ReportFileProgress(string file)
+    {
+        dispatcherQueue.TryEnqueue(() =>
+        {
+            CurrentFileText = file;
+        });
+    }
+
+    public void ReportProgress(int total, int current)
+    {
+        dispatcherQueue.TryEnqueue(() =>
+        {
+            TotalProgress = total;
+            CurrentProgress = current;
+        });
+    }
+
+    public void ReportStatus(string title, string text)
+    {
+        dispatcherQueue.TryEnqueue(() =>
+        {
+            StatusTitle = title;
+            StatusText = text;
+        });
+    }
+
+    [RelayCommand]
+    public void Cancel()
+    {
+        App.GetService<IJobService>().Cancel();
+    }
 }
--- src/BSH.MainApp/Windows/StatusWindow.xaml.cs
+++ src/BSH.MainApp/Windows/StatusWindow.xaml.cs
@@ -1,27 +1,20 @@
 // Copyright (c) Alexander Seeliger. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0.
 
+using BSH.MainApp.Contracts.Services;
 using BSH.MainApp.ViewModels.Windows;
-using WinUIEx;
 
 namespace BSH.MainApp.Windows;
 
 public sealed partial class StatusWindow : WinUIEx.WindowEx
 {
-    public EditBackupViewModel ViewModel { get; set; } = new EditBackupViewModel();
+    public StatusViewModel ViewModel { get; set; } = new StatusViewModel();
 
     public StatusWindow()
     {
         InitializeComponent();
-    }
 
-    public async Task<bool> ShowDialogAsync()
-    {
-        Activate();
-        this.CenterOnScreen();
-        var result = await ViewModel.TaskCompletionSource.Task;
-
-        Close();
-        return result;
+        var statusService = App.GetService<IStatusService>();
+        statusService.AddObserver(ViewModel, true);
     }
 }
