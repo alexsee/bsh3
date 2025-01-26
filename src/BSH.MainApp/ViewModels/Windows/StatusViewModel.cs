af6be9ce6255a8fa8a860a41a3f620e83b88c5c1
Alexander Seeliger <alexander@brightbits.de>
2025-01-26 17:19:55 +0100

    feat: add status window

---
+++ src/BSH.MainApp/ViewModels/Windows/StatusViewModel.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Alexander Seeliger. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BSH.MainApp.ViewModels.Windows;
+public class StatusViewModel : ObservableObject
+{
+
+}
---
+++ src/BSH.MainApp/Windows/StatusWindow.xaml.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Alexander Seeliger. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+
+using BSH.MainApp.ViewModels.Windows;
+using WinUIEx;
+
+namespace BSH.MainApp.Windows;
+
+public sealed partial class StatusWindow : WinUIEx.WindowEx
+{
+    public EditBackupViewModel ViewModel { get; set; } = new EditBackupViewModel();
+
+    public StatusWindow()
+    {
+        InitializeComponent();
+    }
+
+    public async Task<bool> ShowDialogAsync()
+    {
+        Activate();
+        this.CenterOnScreen();
+        var result = await ViewModel.TaskCompletionSource.Task;
+
+        Close();
+        return result;
+    }
+}
